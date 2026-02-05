626a016930e84e9c5f4fe99ff966f2519cc15531
SHUBH1155 <shubhamchauhan0114@gmail.com>
2026-02-05 12:49:18 +0530

    add new sendemails file

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,28 +3,26 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
       secure: false, // TLS
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // Gmail App Password
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
       },
     });
 
-    await transporter.verify(); // connection test
-
     await transporter.sendMail({
-      from: `"Auth System" <${process.env.EMAIL_USER}>`,
+      from: `"Password Reset" <${process.env.SMTP_USER}>`,
       to,
       subject,
       html,
     });
 
-    console.log("✅ Email sent to:", to);
+    console.log("✅ Email sent via Brevo:", to);
   } catch (error) {
-    console.error("❌ Email send error:", error.message);
-    throw error; // important -> 500 visible in logs
+    console.error("❌ Email error:", error);
+    throw error;
   }
 };
 
